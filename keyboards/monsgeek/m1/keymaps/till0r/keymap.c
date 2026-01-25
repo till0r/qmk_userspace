40e48bb9dd08a32f4f5ffb37b4b81d4528c2b861
till0r <till0r@users.noreply.github.com>
2026-01-25 08:56:14 +0100

    Changes pc del to pscr

--- keyboards/monsgeek/m1/keymaps/till0r/keymap.c
+++ keyboards/monsgeek/m1/keymaps/till0r/keymap.c
@@ -39,7 +39,7 @@ enum __custom_keycodes {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     [WIN_B] = LAYOUT_all( /* Base */
-        KC_ESC,  KVM_1,   KVM_2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11, KC_F12,   KC_DEL,           KC_MUTE,
+        KC_ESC,  KVM_1,   KVM_2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11, KC_F12,   KC_PSCR,           KC_MUTE,
         KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS,KC_EQL,   KC_BSPC,          KC_HOME,
         KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC,KC_RBRC,  KC_BSLS,          KC_END,
         KC_ESC,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,KC_NUHS,  KC_ENT,           KC_PGUP,
@@ -55,7 +55,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         _______, _______, _______,                   _______,                            _______,MO(WIN_FN),_______,         KC_A,     KC_S,   KC_D),
 
     [WIN_FN] = LAYOUT_all( /* WASD/↑←↓→ */
-        _______, KC_F1,   KC_F2,   KC_WSCH, KC_WHOM, KC_MSEL, KC_MPLY, KC_MPRV, KC_MNXT, _______, _______, _______, _______, _______,           RM_NEXT,
+        _______, KC_F1,   KC_F2,   KC_WSCH, KC_WHOM, KC_MSEL, KC_MPLY, KC_MPRV, KC_MNXT, _______, _______, _______, _______, KC_DEL,            RM_NEXT,
         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, RM_SPDD, RM_SPDU, _______,           _______,
         _______, _______,TG(WIN_W),_______, _______, _______, _______, _______, KC_INS,  PDF(MAC_B),KC_PSCR,_______, _______, RM_NEXT,           _______,
         _______, _______, _______, _______, _______, _______, _______, _______, _______, RM_TOGG, _______, _______, RM_NEXT, RM_HUEU,           _______,
