c37b44df42e86e78eb06c922a56e8e2adf14e901
till0r <till0r@users.noreply.github.com>
2026-06-12 16:43:03 +0200

    Changes KVM switch to Shift + F1/F2, changes bottom right keys to ISO mapping

--- keyboards/monsgeek/m1/keymaps/till0r/keymap.c
+++ keyboards/monsgeek/m1/keymaps/till0r/keymap.c
@@ -22,9 +22,7 @@ enum __layers {
 };
 
 enum __custom_keycodes {
-  RGB_SLD = SAFE_RANGE,
-  KVM_1,
-  KVM_2
+  RGB_SLD = SAFE_RANGE
 };
 
 #define KC_TASK LGUI(KC_TAB)
@@ -34,15 +32,15 @@ enum __custom_keycodes {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     [WIN_B] = LAYOUT_all( /* Base Layer */
-        KC_ESC,  KVM_1,   KVM_2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11, KC_F12,   KC_PSCR,           KC_MUTE,
+        KC_ESC,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11, KC_F12,   KC_PSCR,          KC_MUTE,
         KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS,KC_EQL,   KC_BSPC,          KC_HOME,
         KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC,KC_RBRC,  KC_BSLS,          KC_END,
         KC_ESC,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,KC_NUHS,  KC_ENT,           KC_PGUP,
         KC_LSFT, KC_NUBS, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,          KC_RSFT, KC_UP,   KC_PGDN,
-        KC_LCTL, KC_LGUI, KC_LALT,                   KC_SPC,                             KC_RGUI, MO(WIN_FN),KC_RALT,        KC_LEFT, KC_DOWN, KC_RGHT),
+        KC_LCTL, KC_LGUI, KC_LALT,                   KC_SPC,                             KC_RALT, MO(WIN_FN),KC_LCTL,        KC_LEFT, KC_DOWN, KC_RGHT),
 
     [WIN_FN] = LAYOUT_all( /* Function Layer */
-        _______, KC_F1,   KC_F2,   KC_WSCH, KC_WHOM, KC_MSEL, KC_MPLY, KC_MPRV, KC_MNXT, _______, _______, _______, _______, KC_DEL,            RM_NEXT,
+        _______, _______, _______, KC_WSCH, KC_WHOM, KC_MSEL, KC_MPLY, KC_MPRV, KC_MNXT, _______, _______, _______, _______, KC_DEL,            RM_NEXT,
         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, RM_SPDD, RM_SPDU, _______,           _______,
         _______, _______, _______, _______, _______, _______, _______, _______, KC_INS,  _______, _______, _______, _______, RM_NEXT,           _______,
         _______, _______, _______, _______, _______, _______, _______, _______, _______, RM_TOGG, _______, _______, RM_NEXT, RM_HUEU,           _______,
@@ -78,20 +76,42 @@ void ctrl_ctrl_2(void) {
     keymap_config.nkro = true;
 }
 
-// Record KVM switching
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
-    switch (keycode) {
-        case KVM_1:
-            if (record->event.pressed) {
-                ctrl_ctrl_1();
-            }
-            return false;
+    uint8_t mods = get_mods();
 
-        case KVM_2:
-            if (record->event.pressed) {
-                ctrl_ctrl_2();
+    switch (keycode) {
+        case KC_F1:
+            if (record->event.pressed && (mods & MOD_MASK_SHIFT)) {
+                // Temporarily clear Shift so KC_1 is not sent as !
+                del_mods(MOD_MASK_SHIFT);
+                send_keyboard_report();
+
+                ctrl_ctrl_1();
+
+                // Restore the original modifiers
+                set_mods(mods);
+                send_keyboard_report();
+
+                return false;
             }
-            return false;
+            return true;
+
+        case KC_F2:
+            if (record->event.pressed && (mods & MOD_MASK_SHIFT)) {
+                // Temporarily clear Shift so KC_2 is not sent as @
+                del_mods(MOD_MASK_SHIFT);
+                send_keyboard_report();
+
+                ctrl_ctrl_2();
+
+                // Restore the original modifiers
+                set_mods(mods);
+                send_keyboard_report();
+
+                return false;
+            }
+            return true;
     }
+
     return true;
 }
